5114e1a5f4b1635e307da09d1feb189fc8987a83
Viet Ta <89030236+viet1012@users.noreply.github.com>
2023-11-13 09:11:58 +0700

    CorsConfig

---
+++ src/main/java/com/ecommerce/WatchStore/Config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.ecommerce.WatchStore.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("development")
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
+
+
