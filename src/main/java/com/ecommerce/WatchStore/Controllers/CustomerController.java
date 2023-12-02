e477988623fbd4aa879ce8339cfc933ba8be2f0c
Viet Ta <89030236+viet1012@users.noreply.github.com>
2023-12-02 11:28:57 +0700

    update customer

--- src/main/java/com/ecommerce/WatchStore/Controllers/CustomerController.java
+++ src/main/java/com/ecommerce/WatchStore/Controllers/CustomerController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/api/Customer")
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
